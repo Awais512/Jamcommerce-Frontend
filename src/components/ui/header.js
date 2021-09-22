b24a87bf2dbedc27a5b7905e847feb0314c040a2
Awais512 <raza.awais25@gmail.com>
2021-09-22 15:57:51 +0500

    Creating Header Part 2

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -6,7 +6,12 @@ import {
   Tab,
   AppBar,
   Toolbar,
+  IconButton,
 } from "@material-ui/core"
+
+import search from "../../images/search.svg"
+import cart from "../../images/cart.svg"
+import account from "../../images/account-header.svg"
 
 const Header = () => {
   return (
@@ -15,6 +20,21 @@ const Header = () => {
         <Button>
           <Typography variant="h1">Var X</Typography>
         </Button>
+        <Tabs>
+          <Tab label="Hats" />
+          <Tab label="Hoodies" />
+          <Tab label="Shirts" />
+          <Tab label="Contact Us" />
+        </Tabs>
+        <IconButton>
+          <img src={search} alt="search" />
+        </IconButton>
+        <IconButton>
+          <img src={cart} alt="cart" />
+        </IconButton>
+        <IconButton>
+          <img src={account} alt="account" />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
--- src/components/ui/layout.js
+++ src/components/ui/layout.js
@@ -6,7 +6,6 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -33,7 +32,7 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer
+        {/* <footer
           style={{
             marginTop: `2rem`,
           }}
@@ -41,14 +40,10 @@ const Layout = ({ children }) => {
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </footer> */}
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
