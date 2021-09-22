48bfad705c81a0c38e1b5c8bb1fff7e39f05f7d3
Awais512 <raza.awais25@gmail.com>
2021-09-22 19:38:19 +0500

    Customizing Material ui components

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -7,20 +7,29 @@ import {
   AppBar,
   Toolbar,
   IconButton,
+  makeStyles,
 } from "@material-ui/core"
 
 import search from "../../images/search.svg"
 import cart from "../../images/cart.svg"
 import account from "../../images/account-header.svg"
 
+const useStyles = makeStyles(theme => ({
+  coloredIndicator: {
+    backgroundColor: "red",
+  },
+}))
+
 const Header = () => {
+  const classes = useStyles()
+
   return (
     <AppBar color="transparent" elevation={0}>
       <Toolbar>
         <Button>
           <Typography variant="h1">Var X</Typography>
         </Button>
-        <Tabs>
+        <Tabs value={0} classes={{ indicator: classes.coloredIndicator }}>
           <Tab label="Hats" />
           <Tab label="Hoodies" />
           <Tab label="Shirts" />
