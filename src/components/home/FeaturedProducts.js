e3d37310465f46b646ddddeb8e27927b48618c2a
Awais512 <raza.awais25@gmail.com>
2021-09-24 22:41:47 +0500

    Featured Product Block 2

--- src/components/home/FeaturedProducts.js
+++ src/components/home/FeaturedProducts.js
@@ -1,5 +1,32 @@
 import React from "react"
+import { Grid, Typography, IconButton, makeStyles } from "@material-ui/core"
 import { useStaticQuery, graphql } from "gatsby"
+import featuredAdornment from "../../images/featured-adornment.svg"
+import frame from "../../images/product-frame-grid.svg"
+
+const useStyles = makeStyles(theme => ({
+  background: {
+    backgroundImage: `url(${featuredAdornment})`,
+    backgroundPosition: "top",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "120rem",
+  },
+  featured: {
+    height: "20rem",
+    width: "20rem",
+  },
+  frame: {
+    backgroundImage: `url(${frame})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    borderRadius: 0,
+    height: "25rem",
+    width: "25rem",
+  },
+}))
 
 const FeaturedProducts = () => {
   const data = useStaticQuery(graphql`
@@ -21,11 +48,28 @@ const FeaturedProducts = () => {
     }
   `)
 
+  const classes = useStyles()
+
   console.log(data)
   return (
-    <div>
-      <h1>Featured Products</h1>
-    </div>
+    <Grid container direction="column" classes={{ root: classes.background }}>
+      {data.allStrapiProduct.edges.map(({ node }, i) => (
+        <Grid item container key={node.strapiId}>
+          <Grid item>
+            <IconButton classes={{ root: classes.frame }}>
+              <img
+                className={classes.featured}
+                src={"http://localhost:1337" + node.variants[0].images[0].url}
+                alt={node.name}
+              />
+            </IconButton>
+            {/* <Grid container direction='column'>
+
+                  </Grid> */}
+          </Grid>
+        </Grid>
+      ))}
+    </Grid>
   )
 }
 
