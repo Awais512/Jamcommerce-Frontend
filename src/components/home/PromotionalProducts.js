592bc156c4b6a0359470315fb0616929b38a3d33
Awais512 <raza.awais25@gmail.com>
2021-09-24 14:28:18 +0500

    Promotional Product section

---
+++ src/components/home/PromotionalProducts.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import Carousel from "react-spring-3d-carousel"
+import clsx from "clsx"
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+import promoAdornment from "../../images/promo-adornment.svg"
+import explore from "../../images/explore.svg"
+
+const useStyles = makeStyles(theme => ({
+  mainContainer: {
+    backgroundImage: `url(${promoAdornment})`,
+    backgroundPosition: "top",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "70rem",
+    padding: "30rem 10rem 10rem 10rem",
+    [theme.breakpoints.down("lg")]: {
+      padding: "20rem 2rem 2rem 2rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      overflow: "hidden",
+    },
+  },
+  productName: {
+    color: "#fff",
+  },
+  iconButton: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  carouselImage: {
+    height: "30rem",
+    width: "25rem",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    boxShadow: theme.shadows[5],
+    [theme.breakpoints.down("sm")]: {
+      height: "25rem",
+      width: "20rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "20rem",
+      width: "15rem",
+    },
+  },
+  carouselContainer: {
+    marginLeft: "20rem",
+    [theme.breakpoints.down("md")]: {
+      marginLeft: 0,
+      height: "30rem",
+    },
+  },
+  space: {
+    margin: "0 15rem 10rem 15rem",
+    [theme.breakpoints.down("sm")]: {
+      margin: "0 8rem 10rem 8rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      margin: "0 5rem 10rem 5rem",
+    },
+  },
+  explore: {
+    textTransform: "none",
+    marginRight: "2rem",
+  },
+  descriptionContainer: {
+    textAlign: "right",
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
+  },
+}))
+
+export default function PromotionalProducts() {
+  const classes = useStyles()
+  const data = useStaticQuery(graphql`
+    query GetPromo {
+      allStrapiProduct(filter: { promo: { eq: true } }) {
+        edges {
+          node {
+            strapiId
+            name
+            description
+            variants {
+              images {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data)
+  const isBrowser = typeof window !== "undefined"
+  const [selectedSlide, setSelectedSlide] = useState(0)
+
+  const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
+  return <h1>Promo Products</h1>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import HeroBlock from "../components/home/HeroBlock"
+import PromotionalProducts from "../components/home/PromotionalProducts"
 
 import Layout from "../components/ui/layout"
 
 const IndexPage = () => (
   <Layout>
     <HeroBlock />
+    <PromotionalProducts />
   </Layout>
 )
 
