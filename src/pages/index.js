862ec109555f0ac7d4a7b475448db83c204617f6
Awais512 <raza.awais25@gmail.com>
2021-09-24 11:19:59 +0500

    Hero Block Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import * as React from "react"
+import HeroBlock from "../components/home/HeroBlock"
 
 import Layout from "../components/ui/layout"
 
-const IndexPage = () => <Layout></Layout>
+const IndexPage = () => (
+  <Layout>
+    <HeroBlock />
+  </Layout>
+)
 
 export default IndexPage
