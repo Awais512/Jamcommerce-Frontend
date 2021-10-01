66a6cfba0ec8f498e1de3245ff909dfa463576f8
Awais512 <raza.awais25@gmail.com>
2021-10-01 22:43:50 +0500

    Category description

--- src/components/product-list/DynamicToolbar.js
+++ src/components/product-list/DynamicToolbar.js
@@ -1,21 +1,23 @@
 import React from "react"
 import { Grid, Typography, makeStyles } from "@material-ui/core"
 import FunctionContainer from "./FunctionContainer"
+import DescriptionContainer from "./DescriptionContainer"
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
     border: `5px solid ${theme.palette.primary.main}`,
     borderRadius: 25,
     width: "95%",
-    height: "20rem",
+    height: "auto",
   },
 }))
 
-const DynamicToolbar = ({ filterOptions }) => {
+const DynamicToolbar = ({ filterOptions, name, description }) => {
   const classes = useStyles()
   return (
     <Grid item container direction="column" classes={{ root: classes.toolbar }}>
       <FunctionContainer filterOptions={filterOptions} />
+      <DescriptionContainer name={name} description={description} />
     </Grid>
   )
 }
--- src/templates/ProductList.js
+++ src/templates/ProductList.js
@@ -3,11 +3,15 @@ import Layout from "../components/ui/layout"
 import { Grid } from "@material-ui/core"
 import DynamicToolbar from "../components/product-list/DynamicToolbar"
 
-const ProductList = ({ pageContext }) => {
+const ProductList = ({ pageContext: { filterOptions, name, description } }) => {
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
-        <DynamicToolbar filterOptions={pageContext.filterOptions} />
+        <DynamicToolbar
+          filterOptions={filterOptions}
+          name={name}
+          description={description}
+        />
       </Grid>
     </Layout>
   )
