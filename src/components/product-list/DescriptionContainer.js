60676af862082589b8e4700fd50e942e269c1a28
Awais512 <raza.awais25@gmail.com>
2021-10-02 16:59:46 +0500

    Adding Layout Button

--- src/components/product-list/DescriptionContainer.js
+++ src/components/product-list/DescriptionContainer.js
@@ -1,6 +1,14 @@
 import React from "react"
-import { Grid, makeStyles, Typography } from "@material-ui/core"
+import {
+  Grid,
+  makeStyles,
+  Typography,
+  ButtonGroup,
+  Button,
+} from "@material-ui/core"
 import background from "../../images/toolbar-background.svg"
+import ListIcon from "../../images/List"
+import GridIcon from "../../images/Grid"
 
 const useStyles = makeStyles(theme => ({
   description: {
@@ -19,6 +27,11 @@ const useStyles = makeStyles(theme => ({
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
+  },
+  button: {
+    border: `2px solid ${theme.palette.primary.main}`,
+    borderRightColor: `${theme.palette.primary.main} !important`,
+    borderRadius: 25,
   },
 }))
 
@@ -42,6 +55,16 @@ const DescriptionContainer = ({ name, description }) => {
         >
           {description}
         </Typography>
+      </Grid>
+      <Grid item>
+        <ButtonGroup>
+          <Button classes={{ outlined: classes.button }}>
+            <ListIcon />
+          </Button>
+          <Button classes={{ outlined: classes.button }}>
+            <GridIcon />
+          </Button>
+        </ButtonGroup>
       </Grid>
     </Grid>
   )
