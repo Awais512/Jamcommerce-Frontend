f4af035eb9e9686decf9297c97ed38f258668d59
Awais512 <raza.awais25@gmail.com>
2021-10-02 21:25:33 +0500

    Category Product Page Query

--- src/templates/ProductList.js
+++ src/templates/ProductList.js
@@ -1,9 +1,14 @@
 import React from "react"
 import Layout from "../components/ui/layout"
 import { Grid } from "@material-ui/core"
+import { graphql } from "gatsby"
 import DynamicToolbar from "../components/product-list/DynamicToolbar"
 
-const ProductList = ({ pageContext: { filterOptions, name, description } }) => {
+const ProductList = ({
+  pageContext: { filterOptions, name, description },
+  data,
+}) => {
+  console.log(data)
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
@@ -18,3 +23,26 @@ const ProductList = ({ pageContext: { filterOptions, name, description } }) => {
 }
 
 export default ProductList
+
+export const query = graphql`
+  query GetCaategoryProducts($id: String) {
+    allStrapiProduct(filter: { category: { id: { eq: $id } } }) {
+      edges {
+        node {
+          strapiId
+          name
+          variants {
+            color
+            id
+            price
+            size
+            style
+            images {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
